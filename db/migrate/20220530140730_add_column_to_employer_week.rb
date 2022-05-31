5f68c539626fe9cdfb2d13adb12b101b7cb41839
yuki <79404325+yuuki0310@users.noreply.github.com>
2022-05-31 21:20:59 +0900

    upgrade

--- db/migrate/20220530140730_add_column_to_employer_week.rb
+++ db/migrate/20220530140730_add_column_to_employer_week.rb
@@ -1,4 +1,4 @@
-railsclass AddColumnToEmployerWeek < ActiveRecord::Migration[5.2]
+class AddColumnToEmployerWeek < ActiveRecord::Migration[5.2]
   def change
     add_column :employer_weeks, :mon, :string,
     add_column :employer_weeks, :tue, :string,
@@ -6,6 +6,6 @@ railsclass AddColumnToEmployerWeek < ActiveRecord::Migration[5.2]
     add_column :employer_weeks, :thu, :string,
     add_column :employer_weeks, :fri, :string,
     add_column :employer_weeks, :sat, :string,
-    add_column :employer_weeks, :sun, :string
+    add_column :employer_weeks, :sun, :string,
   end
 end
