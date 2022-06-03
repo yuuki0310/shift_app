9cf31ce7ff13ff750b0d04d1cb8212614433eee9
yuki <79404325+yuuki0310@users.noreply.github.com>
2022-06-03 23:08:14 +0900

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get "schedules" => "schedules#shop_info"
-  post "schedules/week_set" => "schedules#week_set"
+  get "/ShopInfos" => "ShopInfos#new"
+  # post "/ShopInfos/create" => "ShopInfos#create"
 end
