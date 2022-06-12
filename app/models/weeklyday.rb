7b55ceed6f8870ecf887a02e85b71c94de6cf564
yuki <79404325+yuuki0310@users.noreply.github.com>
2022-06-13 01:39:23 +0900

    update

--- app/models/weeklyday.rb
+++ app/models/weeklyday.rb
@@ -1,4 +1,4 @@
 class Weeklyday < ApplicationRecord
-  has_many :stores, through: :store_schedules
   has_many :store_schedules, dependent: :destroy
+  has_many :stores, through: :store_schedules
 end
