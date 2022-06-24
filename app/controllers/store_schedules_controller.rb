a5736c1bef0b686db930b8242337528061777713
yuki <79404325+yuuki0310@users.noreply.github.com>
2022-06-24 23:17:16 +0900

    update

--- app/controllers/store_schedules_controller.rb
+++ app/controllers/store_schedules_controller.rb
@@ -9,17 +9,28 @@ class StoreSchedulesController < ApplicationController
   end
   
   def create
-    params.require(:store_schedule).permit(weeklyday_id: [])[:weeklyday_id].each do |weeklyday_id|
-      @store_schedule = StoreSchedule.new(
-        store_id: @current_user.id,
-        weeklyday_id: weeklyday_id,
-        working_time_from: storeSchedule_params[:working_time_from],
-        working_time_to: storeSchedule_params[:working_time_to],
-        count: storeSchedule_params[:count]
-      )
-      @store_schedule.save
+    @store_schedule = StoreSchedule.new
+    if params[:store_schedule][:weeklyday_id]
+      params[:store_schedule][:weeklyday_id].each do |weeklyday_id|
+        @store_schedule = StoreSchedule.new(
+          store_id: @current_user.id,
+          weeklyday_id: weeklyday_id,
+          working_time_from: storeSchedule_params[:working_time_from],
+          working_time_to: storeSchedule_params[:working_time_to],
+          count: storeSchedule_params[:count]
+        )
+        @store_schedule.save
+      end
+      if @store_schedule.save == false
+        render("store_schedules/new")
+      else
+        redirect_to("/store_schedules/new")
+      end
+    else
+      storeSchedule_params[:store_id] = @current_user.id
+      @store_schedule = StoreSchedule.create(storeSchedule_params)
+      render("store_schedules/new")
     end
-    redirect_to("/store_schedules/new")
   end
 
   def update
