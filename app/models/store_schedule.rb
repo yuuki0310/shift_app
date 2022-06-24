fe717df10a05a9651f40f5b28bcd58f1d8b8ce56
yuki <79404325+yuuki0310@users.noreply.github.com>
2022-06-24 21:41:09 +0900

    update

--- app/models/store_schedule.rb
+++ app/models/store_schedule.rb
@@ -2,5 +2,5 @@ class StoreSchedule < ApplicationRecord
   belongs_to :store
   # belongs_to :Weeklyday
 
-  # validates :store_id, :weeklyday_id, :working_time_from, :working_time_to, :count, presence: true
+  validates :store_id, :weeklyday_id, :working_time_from, :working_time_to, :count, presence: true
 end
