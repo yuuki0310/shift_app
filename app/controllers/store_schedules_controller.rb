df4f9ddd84d3c997574714796c71a0157bc90c19
yuki <79404325+yuuki0310@users.noreply.github.com>
2022-06-25 18:41:17 +0900

    update

--- app/controllers/store_schedules_controller.rb
+++ app/controllers/store_schedules_controller.rb
@@ -1,24 +1,24 @@
 class StoreSchedulesController < ApplicationController
 
   def storeSchedule_params
-    params.require(:store_schedule).permit(:working_time_from, :working_time_to, :count, :store_id, weeklyday_id: [])
+    params.require(:store_schedule).permit(:working_time_from, :working_time_to, :count, :store_id, weeklyday_id: []).merge(store_id: @current_user.store.id)
   end
   
   def new
     @store_schedule = StoreSchedule.new
+    @working_times = []
+    @current_user.store.store_schedules.each do |store_schedule|
+      @working_times.push(store_schedule.working_time_from)
+      @working_times.push(store_schedule.working_time_to)
+    end
+    @working_times.sort!
   end
   
   def create
     @store_schedule = StoreSchedule.new
     if params[:store_schedule][:weeklyday_id]
       params[:store_schedule][:weeklyday_id].each do |weeklyday_id|
-        @store_schedule = StoreSchedule.new(
-          store_id: @current_user.id,
-          weeklyday_id: weeklyday_id,
-          working_time_from: storeSchedule_params[:working_time_from],
-          working_time_to: storeSchedule_params[:working_time_to],
-          count: storeSchedule_params[:count]
-        )
+        @store_schedule = StoreSchedule.new(storeSchedule_params.merge(weeklyday_id: weeklyday_id))
         @store_schedule.save
       end
       if @store_schedule.save == false
@@ -27,7 +27,6 @@ class StoreSchedulesController < ApplicationController
         redirect_to("/store_schedules/new")
       end
     else
-      storeSchedule_params[:store_id] = @current_user.id
       @store_schedule = StoreSchedule.create(storeSchedule_params)
       render("store_schedules/new")
     end
--- app/models/store_schedule.rb
+++ app/models/store_schedule.rb
@@ -3,4 +3,11 @@ class StoreSchedule < ApplicationRecord
   # belongs_to :Weeklyday
 
   validates :store_id, :weeklyday_id, :working_time_from, :working_time_to, :count, presence: true
+  validate :working_time
+
+  def working_time
+    if working_time_from > working_time_to
+      errors.add(:working_time_from, "可能な時間に設定してください")
+    end
+  end
 end
