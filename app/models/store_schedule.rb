04eb7d23ab6a63acd9aba5346c5b2d41784e13d9
yuki <79404325+yuuki0310@users.noreply.github.com>
2022-06-28 16:31:04 +0900

    update

--- app/models/store_schedule.rb
+++ app/models/store_schedule.rb
@@ -3,11 +3,24 @@ class StoreSchedule < ApplicationRecord
   # belongs_to :Weeklyday
 
   validates :store_id, :weeklyday_id, :working_time_from, :working_time_to, :count, presence: true
-  validate :working_time
+  validate :working_time, :duplicate
 
   def working_time
     if working_time_from > working_time_to
       errors.add(:working_time_from, "可能な時間に設定してください")
+    end
+  end
+end
+
+def duplicate
+  store_schedule_weeklydays = @current_user.store.store_schedules.where(weeklyday_id: weeklyday_id)
+  store_schedule_weeklydays.each do |store_schedule_weeklyday|
+    def decision
+      store_schedule.working_time_from < working_time_from && working_time_from < store_schedule.working_time_to ||
+      store_schedule.working_time_from < working_time_to && working_time_to < store_schedule.working_time_to
+    end
+    if decision
+      errors.add(:weeklyday_id, "重複しています")
     end
   end
 end
