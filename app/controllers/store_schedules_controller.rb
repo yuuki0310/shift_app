b8bdbf77cb4ba46a47a012bf79a6381d9e3b6c89
yuki <79404325+yuuki0310@users.noreply.github.com>
2022-07-01 19:41:01 +0900

    update

--- app/controllers/store_schedules_controller.rb
+++ app/controllers/store_schedules_controller.rb
@@ -5,16 +5,14 @@ class StoreSchedulesController < ApplicationController
   def storeSchedule_params
     params.require(:store_schedule).permit(:working_time_from, :working_time_to, :count, :store_id, weeklyday_id: []).merge(store_id: @current_user.store.id)
   end
-  
-  def new
-    @store_schedule = StoreSchedule.new
+
+  def weekly_scheduled
     @working_times = []
     @current_user.store.store_schedules.each do |store_schedule|
       @working_times.push(store_schedule.working_time_from)
       @working_times.push(store_schedule.working_time_to)
     end
     @working_times.uniq!.sort!
-
     @weeklydays = Weeklyday.all
     def bar_line(weeklyday, working_time)
       store_schedules = @current_user.store.store_schedules.where(weeklyday_id: weeklyday)
@@ -28,8 +26,14 @@ class StoreSchedulesController < ApplicationController
     end
   end
   
+  def new
+    @store_schedule = StoreSchedule.new
+    weekly_scheduled
+  end
+  
   def create
     @store_schedule = StoreSchedule.new
+    weekly_scheduled
     if params[:store_schedule][:weeklyday_id]
       params[:store_schedule][:weeklyday_id].each do |weeklyday_id|
         @store_schedule = StoreSchedule.new(storeSchedule_params.merge(weeklyday_id: weeklyday_id))
@@ -38,12 +42,6 @@ class StoreSchedulesController < ApplicationController
       if @store_schedule.save
         redirect_to("/store_schedules/new")
       else
-        # @working_times = []
-        # @current_user.store.store_schedules.each do |store_schedule|
-        #   @working_times.push(store_schedule.working_time_from)
-        #   @working_times.push(store_schedule.working_time_to)
-        # end
-        # @working_times.uniq!.sort!
         render :new
       end
     else
--- app/models/store_schedule.rb
+++ app/models/store_schedule.rb
@@ -2,12 +2,20 @@ class StoreSchedule < ApplicationRecord
   belongs_to :store
   # belongs_to :Weeklyday
 
-  validates :store_id, :weeklyday_id, :working_time_from, :working_time_to, :count, presence: true
-  # validate :working_time, :duplicate
+  with_options presence: true do
+    validates :store_id
+    validates :weeklyday_id
+    validates :working_time_from
+    validates :working_time_to
+    validates :count
+  end
+  validate :working_time, :duplicate
 
   def working_time
-    if working_time_from > working_time_to
-      errors.add(:working_time_from, "可能な時間に設定してください")
+    if working_time_from && working_time_to
+      if working_time_from > working_time_to
+        errors.add(:working_time_from, "可能な時間に設定してください")
+      end
     end
   end
 
@@ -17,7 +25,7 @@ class StoreSchedule < ApplicationRecord
     store_schedule_weeklydays.each do |store_schedule_weeklyday|
       if store_schedule_weeklyday.working_time_from < working_time_from && working_time_from < store_schedule_weeklyday.working_time_to || \
         store_schedule_weeklyday.working_time_from < working_time_to && working_time_to < store_schedule_weeklyday.working_time_to
-        errors.add(:weeklyday_id, "重複しています")
+        errors.add(:weeklyday_id, "重複しています。可能な時間を設定するか、スケジュールを削除してからもう一度登録してください。")
       end
     end
   end
