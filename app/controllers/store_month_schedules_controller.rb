84719fad2329bee254e2c260f17b1a7d41910606
yuki <79404325+yuuki0310@users.noreply.github.com>
2022-07-09 00:50:22 +0900

    update

--- app/controllers/store_month_schedules_controller.rb
+++ app/controllers/store_month_schedules_controller.rb
@@ -1,17 +1,41 @@
 class StoreMonthSchedulesController < ApplicationController
 
-  helper_method :calendar_wday
+  helper_method :date_table
 
   def storeMonthSchedule_params
     params.require(:store_month_schedule).permit(:date, :working_time_from, :working_time_to, :count, :store_id).merge(store_id: @current_user.store.id)
   end
 
-  def calendar_wday(date)
-    if date.wday == 0
-      return 7
-    else
-      return date.wday
+  
+  def date_table(date)
+    def calendar_wday(date)
+      if date.wday == 0
+        return 7
+      else
+        return date.wday
+      end
     end
+
+    date_tables = []
+    date_schedules = @current_user.store.store_month_schedules.where(date: date)
+    weekly_schedules = @current_user.store.store_schedules.where(weeklyday_id: calendar_wday(date))
+    if date_schedules.present?
+      date_schedules.each do |date_schedule|
+        weekly_schedules.each do |wekkly_schedule|
+          if wekkly_schedule.working_time_from < date_schedule.working_time_from && date_schedule.working_time_from < wekkly_schedule.working_time_to || \
+            wekkly_schedule.working_time_from < date_schedule.working_time_to && date_schedule.working_time_to < wekkly_schedule.working_time_to || \
+            wekkly_schedule.working_time_from == date_schedule.working_time_from && wekkly_schedule.working_time_to == date_schedule.working_time_to
+            date_tables.push(date_schedule)
+          else
+            date_tables.push(wekkly_schedule)
+          end
+        end
+      end
+      date_tables.uniq!
+    else
+      date_tables = weekly_schedules
+    end
+    return date_tables
   end
 
   def new
