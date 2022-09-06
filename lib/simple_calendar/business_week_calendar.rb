b9a1e78166a76bb9db5e474b610478c588acebdf
yuki <79404325+yuuki0310@users.noreply.github.com>
2022-09-07 02:11:35 +0900

    update

--- lib/simple_calendar/business_week_calendar.rb
+++ lib/simple_calendar/business_week_calendar.rb
@@ -2,11 +2,22 @@ class SimpleCalendar::BusinessWeekCalendar < SimpleCalendar::Calendar
   private
 
     def date_range
-      date_range = Date.parse('2022-08-01').to_date..Date.parse('2022-09-01').to_date - 1
+      # date_range = Date.parse('2022-08-01').to_date..Date.parse('2022-09-01').to_date - 1
       # beginning = start_date.beginning_of_week + 1.day
-      beginning = Date.parse('2022-08-01').to_date
       # ending    = start_date.end_of_week - 1.day
+      beginning = Date.parse('2022-08-01').to_date
       ending    = Date.parse('2022-09-01').to_date - 1
-      (beginning..ending).to_a
+      range = (beginning..ending).to_a
+
+      if beginning.wday == 0
+        6.times do
+          range.unshift(nil)
+        end
+      elsif beginning.wday != 1
+        (beginning.wday - 1).times do
+          range.unshift(nil)
+        end
+      end
+      return range    
     end
 end
