9b7fa9f38b43b7e50af4d678254c4a9cacf0675b
yuki <79404325+yuuki0310@users.noreply.github.com>
2022-10-15 02:18:17 +0900

    update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_14_084704) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_29_090706) do
   create_table "affiliation_applications", force: :cascade do |t|
     t.integer "user_id"
     t.integer "store_id"
