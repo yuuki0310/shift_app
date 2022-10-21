f6308034bbc5164fff481fb60f4ff921345f89cd
yuki <79404325+yuuki0310@users.noreply.github.com>
2022-10-21 23:01:07 +0900

    update

--- app/models/shift_section.rb
+++ app/models/shift_section.rb
@@ -2,25 +2,28 @@ class ShiftSection < ApplicationRecord
   belongs_to :store
 
   validates :status, inclusion: { in: 0..3 }
+  with_options presence: true do
+    validates :store_id
+    validates :beginning
+    validates :ending
+    validates :status
+  end
   validate :date, :duplicate
 
   def date
-    if beginning && ending
-      if beginning >= ending
-        errors.add(:beginning, "可能な時間に設定してください")
-      end
+    if beginning && ending && beginning >= ending
+      errors.add(:beginning, "可能な日付に設定してください")
     end
   end
 
   def duplicate
     if beginning && ending
-      current_user_store = Store.find_by(id: store_id)
-      shift_sections = current_user_store.shift_sections
+      shift_sections = Store.find_by(id: store_id).shift_sections
       shift_sections&.each do |shift_section|
-        if shift_section.beginning <= beginning && beginning < shift_section.ending || \
-          shift_section.beginning < ending && ending <= shift_section.ending || \
-          shift_section.beginning == beginning && shift_section.ending == ending
-          errors.add(:date, "重複しています。可能な日付を設定してください。")
+        if shift_section.beginning <= beginning && beginning <= shift_section.ending || \
+          shift_section.beginning <= ending && ending <= shift_section.ending
+          errors.add(:beginning, "重複しています。可能な日付を設定してください。")
+          return
         end
       end
     end
