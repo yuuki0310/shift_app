6759437932ddf0d734c917a95cd2a615d6946e20
yuki <79404325+yuuki0310@users.noreply.github.com>
2022-10-22 18:56:13 +0900

    update

--- app/controllers/user_submissions_controller.rb
+++ app/controllers/user_submissions_controller.rb
@@ -2,7 +2,7 @@ class UserSubmissionsController < ApplicationController
   before_action :logged_in_user, :store_independent, :current_user_authenticate
   
   def create
-    user = User.find_by(params[:user_id])
+    user = User.find(params[:user_id])
     @shift_section = ShiftSection.find_by(store_id: user.store.id, beginning: params[:beginning])
     @submission = UserSubmission.create(user_id: @current_user.id, shift_section_id: @shift_section.id)
     redirect_to "/users/#{params[:user_id]}/user_unable_schedules/#{params[:beginning]}/new"
